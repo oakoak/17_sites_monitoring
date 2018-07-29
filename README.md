5a02ac82d345aa0801d39725f51eee50f214e2c0
varan <varan@VARAN-PC>
2018-07-29 18:49:48 +0300

    add project

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+---
+The script determines if the site is available and up to what date the domain is paid for
+---
+
+## Description
++ The script takes an input file with a list of sites
++ Checks if the site with code 200 is responding
++ Checks up to what date a domain is paid on whois.com
+
+## Example using
+```commandline
+python check_sites_health.py urls.txt
+```
+
+## Example input file *urls.txt*
+```text
+google.ru
+yandex.ru
+stackoverflow.com
+```
+
+## Example output
+```commandline
+Url 'https://yandex.ru' respond with code 200
+Domain 'yandex.ru' paid up before 2018-09-30
+
+Url 'https://stackoverflow.com' respond with code 200
+Domain 'stackoverflow.com' paid up before 2019-02-02
+
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,62 @@
+import requests
+import datetime
+import argparse
+import re
+
+
 def load_urls4check(path):
-    pass
+    with open(path, "r") as file_with_url:
+        return file_with_url.read().split()
+
 
 def is_server_respond_with_200(url):
-    pass
+        page = requests.get(url)
+        return page.ok
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    string_expiration_date = re.findall(
+        r'Expiration Date:</div><div class="df-value">\d{4}-\d{2}-\d{2}',
+        requests.get("https://www.whois.com/whois/{}".format(domain_name)).text
+    )
+    if len(string_expiration_date) > 0:
+        expiration_date = datetime.datetime.strptime(
+            string_expiration_date[0],
+            'Expiration Date:</div><div class="df-value">%Y-%m-%d'
+        )
+        return expiration_date
+    return False
+
+
+def get_parse_args():
+    parser = argparse.ArgumentParser(
+        description="Check the site's response and domain registration date"
+    )
+    parser.add_argument(
+        "path",
+        help="path to the file with urls"
+    )
+
+    return parser.parse_args()
+
+
+def pprint_url_information(url, expiration_date, respond_200):
+    if respond_200:
+        print("Url 'https://{}' respond with code 200".format(url))
+    else:
+        print("Url 'https://{}' not respond with code 200".format(url))
+    print("Domain '{}' paid up before {}\n".format(url, expiration_date.date()))
+
 
 if __name__ == '__main__':
-    pass
+    argument = get_parse_args()
+    try:
+        urls = load_urls4check(argument.path)
+        for url in urls:
+            expiration_date = get_domain_expiration_date(url)
+            respond_200 = is_server_respond_with_200("https://" + url)
+            pprint_url_information(url, expiration_date, respond_200)
+    except FileNotFoundError:
+        exit("Error:file {} not found".format(argument.path))
+    except requests.exceptions.RequestException as error:
+        exit(error)
