aad1c67ddf24fdc51c26038b317c21ce5e3df42f
varan <varan@VARAN-PC>
2018-08-07 08:35:34 +0300

    added use of python-whois packedge

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,7 +1,8 @@
 import requests
 import datetime
 import argparse
-import re
+import whois
+import socket
 
 
 def load_urls4check(path):
@@ -9,23 +10,23 @@ def load_urls4check(path):
         return file_with_url.read().split()
 
 
-def is_server_respond_with_200(url):
+def is_server_respond_with_ok(url):
     page = requests.get(url)
     return page.ok
 
 
 def get_domain_expiration_date(domain_name):
-    string_expiration_date = re.findall(
-        r'Expiration Date:</div><div class="df-value">\d{4}-\d{2}-\d{2}',
-        requests.get("https://www.whois.com/whois/{}".format(domain_name)).text
-    )
-    if string_expiration_date:
-        expiration_date = datetime.datetime.strptime(
-            string_expiration_date[0],
-            'Expiration Date:</div><div class="df-value">%Y-%m-%d'
-        )
-        return expiration_date
-    return False
+    try:
+        response = whois.whois(domain_name)
+        expiration_date = response["expiration_date"]
+        if isinstance(expiration_date, list):
+            return expiration_date[0]
+        else:
+            return expiration_date
+    except socket.error:
+        return False
+    except whois.parser.PywhoisError:
+        return False
 
 
 def get_parse_args():
@@ -34,28 +35,51 @@ def get_parse_args():
     )
     parser.add_argument(
         "path",
+        default="urls.txt",
         help="path to the file with urls"
+    )
+    parser.add_argument(
+        "days",
+        type=int,
+        default=30,
+        help="Amount of days to check the payment date"
     )
 
     return parser.parse_args()
 
 
-def pprint_url_information(url, expiration_date, respond_200):
+def check_payment_date(expiration_date, amount_of_days):
+    date_now = datetime.datetime.today()
+    check_day = date_now + datetime.timedelta(amount_of_days)
+    return check_day < expiration_date
+
+
+def pprint_url_information(url, expiration_date, respond_200, amount_of_days):
     if respond_200:
         print("Url 'https://{}' respond with code 200".format(url))
     else:
         print("Url 'https://{}' not respond with code 200".format(url))
-    print("Domain '{}' paid up before {}\n".format(url, expiration_date.date()))
+    if check_payment_date(expiration_date, amount_of_days):
+        print("Domain '{}' is paid for more then {} days\n"
+              .format(url, amount_of_days))
+    else:
+        print("Domain '{}' isn't paid for {} days\n"
+              .format(url, amount_of_days))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     argument = get_parse_args()
     try:
         urls = load_urls4check(argument.path)
         for url in urls:
             expiration_date = get_domain_expiration_date(url)
-            respond_200 = is_server_respond_with_200("https://" + url)
-            pprint_url_information(url, expiration_date, respond_200)
+            respond_ok = is_server_respond_with_ok("https://" + url)
+            pprint_url_information(
+                url,
+                expiration_date,
+                respond_ok,
+                argument.days
+            )
     except FileNotFoundError:
         exit("Error:file {} not found".format(argument.path))
     except requests.exceptions.RequestException as error:
